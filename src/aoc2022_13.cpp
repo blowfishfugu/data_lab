bf6a39f7ecd609bd6826b9e5c89c745cf5ccd904
blowfishfugu <coolm12345@hotmail.de>
2022-12-12 23:30:26 +0100

    ..day13 of course

--- src/aoc2022_13.cpp
+++ src/aoc2022_13.cpp
@@ -7,6 +7,7 @@ void aoc2022_13()
 	fs::path input(DataDir() / "2022_13.txt");
 	std::ifstream inFile(input);
 	
+
 	__int64 score = 0;
 	for (std::string line; std::getline(inFile, line); ) {
 		if (line.length() == 0) { break; }
