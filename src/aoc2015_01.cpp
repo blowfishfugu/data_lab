538759181480fe9039adc0b894cb7afb46612372
blowfishfugu <coolm12345@hotmail.de>
2023-08-13 17:09:27 +0200

    Day 1: Not Quite Lisp

--- src/aoc2015_01.cpp
+++ src/aoc2015_01.cpp
@@ -1,12 +1,17 @@
 #include "DataDir.h"
 #include <iostream>
+#include <format>
+
+#include <cassert>
 
 void aoc2015_01()
 {
 	fs::path input(DataDir() / "2015_01.txt");
 	
 	std::ifstream inFile(input);
-	__int64 curSum = 0;
+	__int64 floor = 0LL;
+	__int64 current = 1LL;
+	__int64 firstEntered = 0LL;
 	for (std::string line; std::getline(inFile, line);)
 	{
 		if (line.length() == 0)
@@ -14,9 +19,21 @@ void aoc2015_01()
 			break;
 		}
 
-		++curSum;
+		for (const char c : line)
+		{
+			if (c == '(') { ++floor; }
+			if (c == ')') { --floor; }
+			if ( firstEntered==0LL && floor == -1LL )
+			{ 
+				firstEntered = current;
+			}
+			++current;
+		}
+	
 	}
-
-	std::cout << curSum << " Lines \n";
+	std::cout << std::format("final floor: {}\n", floor);
+	std::cout << std::format("entered floor -1 on: {}\n", firstEntered);
+	assert(floor == 232);
+	assert(firstEntered == 1783);
 	
 }
