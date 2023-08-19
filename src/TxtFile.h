f7728a93613110a4ad69cf0b48d5e0f2269d8917
blowfishfugu <coolm12345@hotmail.de>
2023-08-19 18:03:03 +0200

    TxtFile as container of LineIterator

---
+++ src/TxtFile.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "LineIterator.h"
+#include <string>
+#include <fstream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+struct TxtFile {
+	std::string buf{};
+	explicit TxtFile(fs::path input) {
+		auto sz = fs::file_size(input);
+		if (sz > 0) {
+			buf.resize(sz,0);
+			std::ifstream inFile(input);
+			inFile.read(buf.data(), sz);
+			inFile.close();
+			
+			size_t lastChar=buf.find_last_not_of('\0');
+			if ((lastChar+1) < buf.size())
+			{
+				buf.erase((lastChar+1), buf.size() - lastChar);
+			}
+		}
+	}
+
+	LineIterator begin() { return LineIterator{ buf }; }
+	LineIterator end() { return LineIterator{}; }
+};
