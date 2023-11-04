a17bd81f2b5065f5be8e56dec9c5b5641fdb0b0c
blowfishfugu <47995334+blowfishfugu@users.noreply.github.com>
2023-11-04 03:33:11 +0100

    remove #ifdefs from main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,6 @@ extern void join_routine(std::string_view ims, std::string_view paypal);
 
 int main(int argc, char** argv)
 {
-#ifdef UNIT_TEST
-	extern void IteratorCheck();
-	IteratorCheck();
-#endif
-
 	std::cout << "argv0 = \"" << argv[0] << "\"\n";
 	DataDir(argv[0], "paypal");//Set
 
