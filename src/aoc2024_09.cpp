17b05c9269f9dac8d0095d5be4cc719dad800562
blowfishfugu <coolm12345@hotmail.de>
2024-12-09 19:02:31 +0100

    09: Disk Fragmentation, make part1 working again, + multithreading

--- src/aoc2024_09.cpp
+++ src/aoc2024_09.cpp
@@ -1,13 +1,12 @@
 #include "DataDir.h"
 #include "LineIterator.h"
 #include "TxtFile.h"
-#include <charconv>
 #include <iostream>
 #include <print>
 
 namespace {
 	using I = std::int64_t;
-	struct Block {
+	struct Block final{
 		int fileID{ -1 };
 		int chunkSize{0};
 		Block* next{nullptr};
@@ -18,30 +17,34 @@ namespace {
 		return static_cast<int>(c) - '0';
 	}
 
+	auto isNoEmptyBlock = [](Block* emptyCandidate, Block* toFit)->bool {
+		return (emptyCandidate->fileID != -1 || emptyCandidate->chunkSize == 0);
+		};
+
+	auto isNoEmptyFittingBlock = [](Block* emptyCandidate, Block* toFit)->bool {
+		return (emptyCandidate->fileID != -1 || emptyCandidate->chunkSize == 0
+			|| emptyCandidate->chunkSize < toFit->chunkSize //part2 <-fullFit);
+			);
+		};
+
 	struct Disk final{
 		Block* root{};
+		Block* last{};
 		Block* add_block(Block* cursor, int fileID, int chunkSize) {
 			if (cursor && cursor->fileID == fileID) {
 				cursor->chunkSize += chunkSize;
 				return cursor;
 			}
-			Block* next = alloc_block(fileID, chunkSize);
+			Block* next = new Block(fileID, chunkSize, nullptr, cursor);
 			if (!cursor) { root = next; }
 			else {
 				cursor->next = next;
 			}
-			next->prev = cursor;
 			return next;
 		}
 
-		std::vector<Block*> pool;
-		Block* alloc_block(int fileID, int chunkSize) {
-			Block* b = new Block{ fileID,chunkSize,nullptr,nullptr };
-			pool.push_back(b);
-			return b;
-		}
-
-		bool repartition_block(Block* from, Block* emptyBlock) {
+		template<typename FitCondition>
+		bool repartition_block(Block* from, Block* emptyBlock, FitCondition isNoEmpty) {
 			if (from == nullptr) {
 				return false;
 			}
@@ -51,30 +54,23 @@ namespace {
 
 			int initialSize = from->chunkSize;
 			while (from->chunkSize > 0) {
-				while (emptyBlock->fileID != -1 || emptyBlock->chunkSize==0 
-					|| emptyBlock->chunkSize < from->chunkSize //part2 <-fullFit
-					) {
+				while (isNoEmpty(emptyBlock,from) ) {
 					emptyBlock = emptyBlock->next;
-					if (emptyBlock == from)
-					{
+					if (emptyBlock == from){
 						return false; //reached end
 					}
 				}
-
-
 
 				Block* target = nullptr;
 				if (emptyBlock->prev != nullptr && emptyBlock->prev->fileID == from->fileID) {
 					target = emptyBlock->prev;
 				}
 				else {
-					target = alloc_block(from->fileID, 0);
+					target = new Block(from->fileID, 0, emptyBlock, emptyBlock->prev);
 					if (emptyBlock->prev != nullptr)
 					{
 						emptyBlock->prev->next = target;
 					}
-					target->prev = emptyBlock->prev;
-					target->next = emptyBlock;
 					emptyBlock->prev = target;
 				}
 
@@ -94,14 +90,12 @@ namespace {
 		void print() const {
 			Block* current = root;
 			while (current != nullptr) {
-				if (current->fileID != -1)
-				{
+				if (current->fileID != -1) {
 					for (size_t i = 0; i < current->chunkSize; ++i) {
 						std::print(std::cout, "{}", current->fileID);
 					}
 				}
-				else
-				{
+				else {
 					for (size_t i = 0; i < current->chunkSize; ++i) {
 						std::print(std::cout, "{}", '.');
 					}
@@ -117,8 +111,7 @@ namespace {
 			Block* current = root;
 			while (current != nullptr) {
 				for (size_t i = 0; i < current->chunkSize; ++i) {
-					if (current->fileID != -1)
-					{
+					if (current->fileID != -1) {
 						I prod = index * current->fileID;
 						sum += prod;
 					}
@@ -130,7 +123,11 @@ namespace {
 		}
 
 		void format() {
-			for (Block* b : pool) { delete b; }
+			while (root) {
+				Block* current = root;
+				root = root->next;
+				delete current;
+			}
 		}
 		~Disk(){
 			this->format();
@@ -144,52 +141,66 @@ void aoc2024_09()
 	TxtFile txt{ input };
 
 	Disk disk{};
+	Disk disk2{};
 	int fileIDs{}; //linesize/2?
 	int fileSum{};
 	int freeSum{};
-	Block* cursor = nullptr;
-	for (const auto& line : txt)
-	{
+	
+	for (const auto& line : txt) {
 		if (line.length() == 0) { break; }
 
 		bool isEmpty = false;
 		for (size_t i = 0; i < line.size(); ++i) {
 			int sz = to_int(line[i]);
 			if (!isEmpty){
-				cursor = disk.add_block(cursor, fileIDs, sz);
+				disk.last = disk.add_block(disk.last, fileIDs, sz);
+				disk2.last = disk2.add_block(disk2.last, fileIDs, sz);
 				fileIDs++;
 				fileSum += sz;
 			}
 			else {
-				cursor = disk.add_block(cursor, -1, sz);
+				disk.last = disk.add_block(disk.last, -1, sz);
+				disk2.last = disk2.add_block(disk2.last, -1, sz);
 				freeSum += sz;
 			}
 
 			isEmpty = !isEmpty;
 		}
 	}
-
-	//disk.print();
-
-	Block* lastBlock = cursor;
-	Block* nextEmpty = disk.root;
-	while (lastBlock != disk.root )
-	{
-		disk.repartition_block(lastBlock, nextEmpty);
-		//disk.print();
-		if (!lastBlock)
-		{ 
-			break; 
-		}
-		lastBlock = lastBlock->prev;
-	}
-
-	//disk.print();
-	std::println(std::cout, "checksum {}", disk.checksum());
+	
 	std::println(std::cout, "fileIDs: {}", fileIDs);
 	std::println(std::cout, "fileSum: {}", fileSum);
 	std::println(std::cout, "freeSum: {}", freeSum);
 	std::println(std::cout, "diskSize: {}", fileSum+freeSum);
+	//disk.print();
+
+	std::jthread th1([&disk]() {
+		Block* lastBlock = disk.last;
+		Block* nextEmpty = disk.root;
+		while (lastBlock != disk.root && lastBlock != nullptr) {
+			disk.repartition_block(lastBlock, nextEmpty, isNoEmptyBlock);
+			//disk.print();
+			lastBlock = lastBlock->prev;
+		}
+		}
+	);
+
+	std::jthread th2([&disk2]() {
+		Block* lastBlock = disk2.last;
+		Block* nextEmpty = disk2.root;
+		while (lastBlock != disk2.root && lastBlock != nullptr)
+		{
+			disk2.repartition_block(lastBlock, nextEmpty, isNoEmptyFittingBlock);
+			//disk.print();
+			lastBlock = lastBlock->prev;
+		}
+		}
+	);
+
+	th1.join();
+	std::println(std::cout, "checksum Disk1 {}", disk.checksum());
+	th2.join();
+	std::println(std::cout, "checksum Disk2 {}", disk2.checksum());
 	
 }
 
