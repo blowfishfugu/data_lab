f315cb0363b94db8a9f2a9cc9454b3be7ceb96df
blowfishfugu <coolm12345@hotmail.de>
2024-12-22 19:03:10 +0100

    22: monkey business, combine sequences

--- src/aoc2024_22.cpp
+++ src/aoc2024_22.cpp
@@ -9,6 +9,28 @@
 #include <numeric>
 #include <algorithm>
 #include <execution>
+#include <atomic>
+#include <map>
+#include <unordered_map>
+
+using Sequence = std::tuple<int, int, int, int>;
+using SeqMap = std::unordered_map<Sequence, int>;
+
+template <>
+class std::hash<Sequence>
+{
+public:
+	std::uint64_t operator()(const Sequence& seq) const
+	{
+		std::uint64_t h = (
+			std::get<0>(seq) << sizeof(int)   *3 
+			^ std::get<1>(seq) << sizeof(int) *2 
+			^ std::get<2>(seq) << sizeof(int)
+			^ std::get<3>(seq)
+			);
+		return h;
+	}
+};
 
 namespace D22 {
 	using UI = std::uint64_t;
@@ -53,11 +75,45 @@ namespace D22 {
 		return secret;
 	};
 
-	constexpr auto loop = [](UI& secret, const UI rounds) {
+	constexpr auto toPrice = [](const UI secret)->int {
+		UI overTen = static_cast<UI>(std::floor((double)secret / 10.0)*10);
+		return static_cast<int>(secret - overTen);
+		};
+	
+
+	
+
+	constexpr auto loop = [](UI& secret, const UI rounds)->SeqMap {
+		using Price = std::tuple<int, int>;
+		using Prices = std::vector<Price>;
+		Prices p{}; p.reserve(rounds+1);
+		p.emplace_back(Price{ toPrice(secret),0 });
 		for (UI i = 0; i < rounds; ++i) {
 			secret = buildNext(secret);
 			//std::println(std::cout, "{:>4} : {}", i, secret );
+			p.emplace_back(Price{toPrice(secret),0});
 		}
+
+		for (int i = 1; i < p.size(); ++i) {
+			const Price& last = p[i - 1];
+			auto& [current, delta] = p[i];
+			delta = current - std::get<0>(last);
+		}
+
+		SeqMap seqs{};
+		for (int i = 4; i < p.size(); ++i) {
+			Sequence s{ 
+				std::get<1>(p[i-3]),
+				std::get<1>(p[i-2]),
+				std::get<1>(p[i-1]),
+				std::get<1>(p[i-0]),
+			};
+			if (!seqs.contains(s)) {
+				seqs.emplace(s, std::get<0>(p[i - 0]));
+			}
+		}
+
+		return seqs;
 	};
 
 }
@@ -68,11 +124,10 @@ void aoc2024_22()
 	fs::path input(DataDir() / "2024_22.txt");
 	TxtFile txt{ input };
 
-	UI tstXor = mix(42, 15);
-	assert(tstXor == 37);
-
-	UI tstPrune = prune(100000000);
-	assert(tstPrune == 16113920);
+	///*UI tstXor = mix(42, 15);
+	//assert(tstXor == 37);
+	//UI tstPrune = prune(100000000);
+	//assert(tstPrune == 16113920);*/
 
 	__int64 count = 0LL;
 	VI numbers{};
@@ -83,12 +138,27 @@ void aoc2024_22()
 		++count;
 	}
 	
+	std::vector<SeqMap> monkeys;
+	monkeys.resize(numbers.size());
+	std::atomic<int> index{0};
 	std::for_each( std::execution::par,
-		numbers.begin(), numbers.end(), [](UI& secret) {
-		loop(secret, 2000LL);
+		numbers.begin(), numbers.end(), [&index,&monkeys](UI& secret) {
+		int i = index.fetch_add(1, std::memory_order_relaxed);
+		monkeys[i]= loop(secret, 2000LL);
 	});
 
 	UI sum=std::accumulate(numbers.begin(), numbers.end(), 0ULL);
 	std::println(std::cout, "sum of secrets: {}", sum);
+
+	int maxBananas{};
+	SeqMap combined{};
+	for (int i = 0; i < monkeys.size(); ++i) {
+		const SeqMap& l = monkeys[i];
+		for ( const auto& [seq, prize] : l) {
+			combined[seq] += prize;
+			maxBananas = std::max(maxBananas, combined[seq]);
+		}
+	}
+	std::println(std::cout, "maxBananas: {}", maxBananas);
 }
 
